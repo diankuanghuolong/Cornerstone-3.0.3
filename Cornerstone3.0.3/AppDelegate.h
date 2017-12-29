cdf9e762496ff35261c6e68c5f90cb2b62c0f33b
Ios Developer <ios_developer@ios-developerdemacbook-pro.local>
2017-12-29 13:37:12 +0800

    破解版

---
+++ Cornerstone3.0.3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cornerstone3.0.3
+//
+//  Created by Ios_Developer on 2017/12/29.
+//  Copyright © 2017年 hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
